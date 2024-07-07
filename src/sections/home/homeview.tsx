b39a8431c598ad63c7c295cc5384368c0a18a2f2
dinh.thi.thu.ha <dinh.thi.thu.ha@sun-asterisk.com>
2024-07-07 16:13:13 +0700

    .

--- src/sections/home/homeview.tsx
+++ src/sections/home/homeview.tsx
@@ -9,16 +9,16 @@ export default function HomeView() {
     return (
         <Container maxWidth="xl">
             <Typography variant="h4" sx={{ mb: 5 }}>
-                Hi, Welcome Home
+                Xin chào Quản trị viên
             </Typography>
             <Grid container spacing={5}>
-                <Grid xs={12} sm={6} md={3}>
+                {/* <Grid xs={12} sm={6} md={3}>
                     <AppWidgetSummary
                         title="Thành phố"
                         total={60}
                         icon={<img alt="icon" src="/assets/icons/city.png" />}
                     />
-                </Grid>
+                </Grid> */}
                 <Grid xs={12} sm={6} md={3}>
                     <AppWidgetSummary
                         title="Khách sạn"
@@ -33,69 +33,28 @@ export default function HomeView() {
                         icon={<img alt="icon" src="/assets/icons/user.png" />}
                     />
                 </Grid>
+
                 <Grid xs={12} sm={6} md={3}>
                     <AppWidgetSummary
-                        title="Tổng doanh thu"
+                        title="Lượt đặt phòng/tháng"
+                        total={17000}
+                        icon={<img alt="icon" src="/assets/icons/city.png" />}
+                    />
+                </Grid>
+
+                <Grid xs={12} sm={6} md={3}>
+                    <AppWidgetSummary
+                        title="Doanh thu/tháng"
                         total={17000}
                         icon={<img alt="icon" src="/assets/icons/total_revenue.png" />}
                     />
                 </Grid>
 
-                {/* <Grid xs={12} sm={6} md={3}>
-                    <AppWidgetSummary
-                        title="Bug Reports"
-                        total={234}
-                        color="error"
-                        icon={<img alt="icon" src="/assets/icons/glass/ic_glass_message.png" />}
-                    />
-                </Grid> */}
-
-                <Grid xs={12} md={6} lg={6}>
-                    <AppRevenueMonth
-                        title="Website Visits"
-                        subheader="(+43%) than last year"
-                        chart={{
-                            labels: [
-                                '01/01/2003',
-                                '02/01/2003',
-                                '03/01/2003',
-                                '04/01/2003',
-                                '05/01/2003',
-                                '06/01/2003',
-                                '07/01/2003',
-                                '08/01/2003',
-                                '09/01/2003',
-                                '10/01/2003',
-                                '11/01/2003',
-                            ],
-                            series: [
-                                {
-                                    name: 'Team A',
-                                    type: 'column',
-                                    fill: 'solid',
-                                    data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
-                                },
-                                {
-                                    name: 'Team B',
-                                    type: 'area',
-                                    fill: 'gradient',
-                                    data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43],
-                                },
-                                {
-                                    name: 'Team C',
-                                    type: 'line',
-                                    fill: 'solid',
-                                    data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39],
-                                },
-                            ],
-                        }}
-                    />
-                </Grid>
-                <Grid xs={12} md={6} lg={6} bgcolor="#FFF">
+                <Grid xs={12} md={6} lg={5} bgcolor="#FFF">
                     <Table>
                         <TableHead>
                             <TableRow>
-                                <TableCell sx={{ padding: "8px" }}>
+                                <TableCell sx={{ padding: "8px", width: "320px" }}>
                                     <Typography>Tên khách sạn</Typography>
                                 </TableCell>
                                 <TableCell sx={{ padding: "8px" }} align="center">
@@ -107,13 +66,12 @@ export default function HomeView() {
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {/* {topShops.map((topShop, key) => ( */}
                             <TableRow >
-                                <TableCell sx={{ display: "flex", gap: 2, p: "10px" }}>
+                                <TableCell sx={{ display: "flex", gap: 2, p: "10px", width: "320px" }}>
                                     <Avatar src="https://cdn-icons-png.freepik.com/512/147/147142.png"></Avatar>
-                                    <Box>
-                                        <Typography fontWeight={550}>Khách sạn Mường Thanh</Typography>
-                                        <Typography fontSize={14} color="#7E7988">
+                                    <Box overflow="hidden">
+                                        <Typography fontWeight={550} whiteSpace="nowrap" overflow="hidden" textOverflow="ellipsis">Khách sạn Mường Thanh Khách sạn Mường Thanh Khách sạn Mường Thanh</Typography>
+                                        <Typography fontSize={14} color="#7E7988" whiteSpace="nowrap" overflow="hidden" textOverflow="ellipsis">
                                             Số 20 Nguyễn Xiển
                                         </Typography>
                                     </Box>
@@ -144,19 +102,63 @@ export default function HomeView() {
                     </Table>
                 </Grid>
 
-                {/* <Grid xs={12} md={6} lg={4}>
-                    <AppCurrentVisits
-                        title="Current Visits"
-                        chart={{
-                            series: [
-                                { label: 'America', value: 4344 },
-                                { label: 'Asia', value: 5435 },
-                                { label: 'Europe', value: 1443 },
-                                { label: 'Africa', value: 4443 },
-                            ],
-                        }}
-                    />
-                </Grid> */}
+                <Grid xs={12} md={6} lg={7} bgcolor="#FFF">
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell sx={{ padding: "8px", width: "40%" }}>
+                                    <Typography>Tên khách sạn</Typography>
+                                </TableCell>
+                                <TableCell sx={{ padding: "8px", width: "20%" }} align="center">
+                                    <Typography>Hotline</Typography>
+                                </TableCell>
+                                <TableCell sx={{ padding: "8px", width: "20%" }} align="center">
+                                    <Typography>Lượt đặt phòng</Typography>
+                                </TableCell>
+                                <TableCell sx={{ padding: "8px", width: "20%" }} align="center">
+                                    <Typography>Doanh thu</Typography>
+                                </TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            <TableRow >
+                                <TableCell sx={{ display: "flex", gap: 2, p: "10px", width: "350px", flexDirection: "row" }}>
+                                    <Avatar src="https://cdn-icons-png.freepik.com/512/147/147142.png"></Avatar>
+                                    <Box display="flex" flexDirection="column" overflow="hidden">
+                                        <Typography fontWeight={550} whiteSpace="nowrap" overflow="hidden" textOverflow="ellipsis" >Khách sạn Mường Thanh Khách Mường Thanh Khách </Typography>
+                                        <Typography fontSize={14} color="#7E7988" whiteSpace="nowrap" overflow="hidden" textOverflow="ellipsis" >
+                                            Số 20 Nguyễn Xiển Số 20 Nguyễn Xiển Số 20 Nguyễn Xiển Số 20 Nguyễn Xiển
+                                        </Typography>
+                                    </Box>
+                                </TableCell>
+                                <TableCell sx={{ p: "10px", width: "20%" }} align="center">
+                                    <Typography>02435738964</Typography>
+                                </TableCell>
+                                <TableCell sx={{ p: "10px", width: "20%" }} align="center">
+                                    <Typography>15</Typography>
+                                </TableCell>
+                                <TableCell sx={{ p: "10px", width: "20%" }}>
+                                    <Box
+                                        display="flex"
+                                        borderRadius={10}
+                                        bgcolor="orange"
+                                        alignItems="center"
+                                        justifyContent="center"
+                                        p="4px"
+
+                                        gap={0.5}
+                                        m="auto"
+                                    >
+                                        <Typography fontWeight="600">
+                                            20.000.000
+                                        </Typography>
+                                    </Box>
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </Grid>
+
             </Grid>
         </Container >
     )
